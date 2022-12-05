18269f8bcedb01576d8d801e80853661a701a46f
Pavel R <mjk1_126@mail.ru>
2022-12-05 14:54:29 +0300

    style: delete unused import

--- stats-server/src/main/java/ru/practicum/statistics/repository/StatisticsRepository.java
+++ stats-server/src/main/java/ru/practicum/statistics/repository/StatisticsRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.statistics.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.statistics.model.Hits;
 
@@ -11,11 +10,5 @@ import java.util.List;
 @Repository
 public interface StatisticsRepository extends JpaRepository<Hits, Long> {
     List<Hits> findAllByAppAndUri(String app, String uri);
-
     List<Hits> findAllByTimestampBetweenAndUriIn(LocalDateTime start, LocalDateTime end, List<String> uris);
-    @Query("select h from Hits h " +
-            "where h.timestamp >= ?1 " +
-            "and h.timestamp <= ?2 " +
-            "and h.uri in ?3 ")
-    List<Hits> findAll(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
--- stats-server/src/main/java/ru/practicum/statistics/service/StatisticsServiceImpl.java
+++ stats-server/src/main/java/ru/practicum/statistics/service/StatisticsServiceImpl.java
@@ -13,7 +13,6 @@ import ru.practicum.statistics.repository.StatisticsRepository;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
