3fb3a29719f81b747da0df788825599c6ecd8823
Pavel R <mjk1_126@mail.ru>
2022-12-05 14:57:02 +0300

    style: refactor another style problem

--- stats-server/src/main/java/ru/practicum/statistics/repository/StatisticsRepository.java
+++ stats-server/src/main/java/ru/practicum/statistics/repository/StatisticsRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface StatisticsRepository extends JpaRepository<Hits, Long> {
     List<Hits> findAllByAppAndUri(String app, String uri);
+
     List<Hits> findAllByTimestampBetweenAndUriIn(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
